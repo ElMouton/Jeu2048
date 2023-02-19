49ac6c40dabd9cfea1f38b512a6b217d2824838a
Julian <herterjulian88@gmail.com>
2023-02-19 01:55:57 +0100

    Création des méthodes de Jeu.java

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -2,12 +2,37 @@ package jeu;
 
 public class Jeu extends SujetObserve{
     private int size;
+    private int objectif;
 
     public Jeu() {
         this.size = 4;
     }
 
+    public void jouer(int l, int c){
+
+    }
+
     public int size() {
         return size;
+    }
+
+    public int getCase(int l, int c){
+        return 0;
+    }
+
+    public int getNbGagnees(){
+        return 0;
+    }
+
+    public int getNbJouees(){
+        return 0;
+    }
+
+    public void setTaille(int taille){
+        this.size = taille;
+    }
+
+    public void setObjectif(int objectif){
+        this.objectif = objectif;
     }
 }
