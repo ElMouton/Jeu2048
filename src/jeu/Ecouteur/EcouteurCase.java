b8fc1ccab1bd56e6b5f7dbd8766e0d897663888d
Julian <herterjulian88@gmail.com>
2023-03-01 23:22:00 +0100

    Objectif fonctionnel + Jeu fini

--- src/jeu/Ecouteur/EcouteurCase.java
+++ src/jeu/Ecouteur/EcouteurCase.java
@@ -26,28 +26,21 @@ public class EcouteurCase implements EventHandler {
         if (this.col != 0) haut = this.jeu.getCase(this.lig, this.col - 1);
         if (this.col != this.jeu.size() - 1) bas = this.jeu.getCase(this.lig, this.col + 1);
 
-        System.out.println("col = " + lig + " lig = " + col + " chiffreAct = " + chiffreAct);
-        System.out.println("haut = " + haut + " bas = " + bas + " gauche = " + gauche + " droite = " + droite);
-
         boolean find = false;
         if (chiffreAct == haut) {
             find = true;
-            System.out.println("oui haut");
             this.jeu.nouveauNombre(this.lig, this.col - 1);
         }
         if (chiffreAct == bas) {
             find = true;
-            System.out.println("oui bas");
             this.jeu.nouveauNombre(this.lig, this.col + 1);
         }
         if (chiffreAct == gauche) {
             find = true;
-            System.out.println("oui gauche");
             this.jeu.nouveauNombre(this.lig - 1, this.col);
         }
         if (chiffreAct == droite) {
             find = true;
-            System.out.println("oui droite");
             this.jeu.nouveauNombre(this.lig + 1, this.col);
         }
 
